2cf2d15961b454bf554121a146cc02ccc0ca078a
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2023-06-05 08:52:21 +0800

    fix to camera script

--- asset/script/camera.js
+++ asset/script/camera.js
@@ -1,36 +1,44 @@
 navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function(stream) {
-        var video = document.createElement('video');
-        video.srcObject = stream;
-        video.play();
+  .then(function(stream) {
+    var video = document.createElement('video');
+    video.srcObject = stream;
 
-        // Create a canvas element
-        var canvas = document.getElementById('canvas');
-        var context = canvas.getContext('2d');
+    // Listen for the 'play' event to start drawing frames
+    video.addEventListener('play', function() {
+      var canvas = document.getElementById('canvas');
+      var context = canvas.getContext('2d');
+      var track = stream.getVideoTracks()[0];
+      var imageCapture = new ImageCapture(track);
 
-        // Create an ImageCapture object from the video stream
-        var track = stream.getVideoTracks()[0];
-        var imageCapture = new ImageCapture(track);
+      function drawFrame() {
+        imageCapture.grabFrame()
+          .then(function(imageBitmap) {
+            canvas.width = imageBitmap.width;
+            canvas.height = imageBitmap.height;
+            context.drawImage(imageBitmap, 0, 0);
+          })
+          .catch(function(error) {
+            console.log('Error grabbing frame: ', error);
+          });
 
-        // Draw the webcam image on the canvas every time a new frame is available
-        function drawFrame() {
-            imageCapture.grabFrame()
-                .then(function(imageBitmap) {
-                    canvas.width = imageBitmap.width;
-                    canvas.height = imageBitmap.height;
-                    context.drawImage(imageBitmap, 0, 0);
-                    requestAnimationFrame(drawFrame);
-                })
-                .catch(function(error) {
-                    console.log('Error grabbing frame: ', error);
-                });
-        }
+        // Schedule the next frame
+        setTimeout(drawFrame, 0);
+      }
 
-        drawFrame();
-    })
-    .catch(function(error) {
-        console.log('Error accessing webcam: ', error);
+      drawFrame();
     });
+
+    // Start playing the video
+    video.play()
+      .catch(function(error) {
+        console.log('Error playing video: ', error);
+      });
+  })
+  .catch(function(error) {
+    console.log('Error accessing webcam: ', error);
+  });
+
+  
 
     function checkVideoInput() {
         return new Promise(function(resolve, reject) {
