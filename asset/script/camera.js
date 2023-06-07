d895e2d5066f4cef7ebb9d42b7d27315c207a270
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2023-06-07 20:11:24 +0800

    some initial significant changes

--- asset/script/camera.js
+++ asset/script/camera.js
@@ -1,151 +1,25 @@
-const cameraCanvas = document.querySelector('#canvas');
-const camLoading = document.querySelector('#camLoading');
-let isMobile = true;
-let isStreaming = false;
-let constraints = { video: { facingMode: "user" }, audio: false };
-
-function toggleCamera() {
-  if (isMobile) {
-    if (constraints.video.facingMode == "user") {
-      constraints.video.facingMode = "environment";
-    } else {
-      constraints.video.facingMode = "user";
-    }
-    reinitializeCamera();
+function hasGetUserMedia() {
+    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
   }
-}
-
-function checkIfMobileBrowser() {
-  isMobile = /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  isMobile ? (constraints.video = { facingMode: "environment" }) : (constraints.video = true);
-  return isMobile;
-}
-
-function initializeCamera() {
-  checkIfMobileBrowser();
-  navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
-    let cameraTrack = stream.getTracks()[0];
-    let imageCapture = new ImageCapture(cameraTrack);
-    isStreaming = true;
-    let context = cameraCanvas.getContext('2d');
-    function drawFrame() {
-      imageCapture.grabFrame().then(function (imageBitmap) {
-        let cameraRatio = imageBitmap.width / imageBitmap.height;
-        cameraCanvas.width = window.innerWidth;
-        cameraCanvas.height = window.innerHeight * (1 + cameraRatio);
-        context.drawImage(imageBitmap, 0, 0, cameraCanvas.width, cameraCanvas.height);
-        requestAnimationFrame(drawFrame);
-      }).catch(function (error) {
-        console.log('Error grabbing frame: ', error);
+  
+  function enableCam() {
+    if (hasGetUserMedia()) {
+      // getUsermedia parameters.
+      const constraints = {
+        video: true,
+        width: 640, 
+        height: 480 
+      };
+  
+      // Activate the webcam stream.
+      navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+        VIDEO.srcObject = stream;
+        VIDEO.addEventListener('loadeddata', function() {
+          videoPlaying = true;
+          ENABLE_CAM_BUTTON.classList.add('removed');
+        });
       });
-    }
-    drawFrame();
-  }).catch(function (error) {
-    console.log('Error accessing webcam: ', error);
-  });
-  setInterval(() => {
-    if (isStreaming) {
-      document.querySelector('#camLoading').style.display = 'none';
     } else {
-      document.querySelector('#camLoading').style.display = 'inherit';
+      console.warn('getUserMedia() is not supported by your browser');
     }
-  }, 1000);
-}
-
-function reinitializeCamera() {
-  isStreaming = false;
-  initializeCamera();
-}
-
-document.addEventListener('DOMContentLoaded', initializeCamera());
-
-
-/*
-function getStream() {
-  if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
-    return navigator.mediaDevices.enumerateDevices()
-      .then(function(devices) {
-        var rearCamera = devices.find(function(device) {
-          return device.kind === 'videoinput' && device.facingMode === 'environment';
-        });
-
-        var constraints = { video: true };
-        if (rearCamera) {
-          constraints.video = { deviceId: rearCamera.deviceId };
-        }
-
-        return navigator.mediaDevices.getUserMedia(constraints);
-      });
-  } else {
-    console.log('getUserMedia is not supported');
-    return Promise.reject(new Error('getUserMedia is not supported'));
   }
-}
-
-getStream()
-  .then(function(stream) {
-    var video = document.createElement('video');
-    video.srcObject = stream;
-
-    // Listen for the 'play' event to start drawing frames
-    video.addEventListener('play', function() {
-      var canvas = document.getElementById('canvas');
-      var context = canvas.getContext('2d');
-      var track = stream.getVideoTracks()[0];
-      var imageCapture = new ImageCapture(track);
-
-      function drawFrame() {
-        imageCapture.grabFrame()
-          .then(function(imageBitmap) {
-              let screen_ratio = imageBitmap.width / imageBitmap.height;
-              canvas.width = window.height;
-              canvas.height = window.width * screen_ratio;
-              context.drawImage(imageBitmap, 0, 0);
-          })
-          .catch(function(error) {
-            console.log('Error grabbing frame: ', error);
-          });
-
-        // Schedule the next frame
-        requestAnimationFrame(drawFrame);
-      }
-
-      drawFrame();
-    });
-
-    // Start playing the video
-    video.play()
-      .catch(function(error) {
-        console.log('Error playing video: ', error);
-      });
-  })
-  .catch(function(error) {
-    console.log('Error accessing webcam: ', error);
-  });
-
-
-    function checkVideoInput() {
-        return new Promise(function(resolve, reject) {
-          navigator.mediaDevices.getUserMedia({ video: true })
-            .then(function(stream) {
-              // Video input retrieved successfully
-              resolve(true);
-              stream.getTracks().forEach(function(track) {
-                track.stop(); // Stop the media stream tracks
-              });
-            })
-            .catch(function(error) {
-              // Failed to retrieve video input
-              resolve(false);
-            });
-        });
-      }
-    
-    // Usage example
-checkVideoInput()
-    .then(function(hasVideoInput) {
-        hasVideoInput ?  
-        document.querySelector('#camLoading').style.display = 'none' : 
-        document.querySelector('#camLoading').style.display = 'inherit';
-});
-*/
--- asset/script/scanner.js
+++ asset/script/scanner.js
@@ -3,3 +3,93 @@ function backspace(){
 
 function enter(){
 }
+
+const STATUS = document.getElementById('status');
+const VIDEO = document.getElementById('camera');
+const ENABLE_CAM_BUTTON = document.getElementById('enableCam');
+const RESET_BUTTON = document.getElementById('reset');
+const TRAIN_BUTTON = document.getElementById('train');
+const MOBILE_NET_INPUT_WIDTH = 224;
+const MOBILE_NET_INPUT_HEIGHT = 224;
+const STOP_DATA_GATHER = -1;
+const CLASS_NAMES = [];
+
+const MODEL_URL = '?';
+
+let mobilenet = undefined;
+let gatherDataState = STOP_DATA_GATHER;
+let videoPlaying = false;
+let trainingDataInputs = [];
+let trainingDataOutputs = [];
+let examplesCount = [];
+let predict = false;
+
+let dataCollectorButtons = document.querySelectorAll('button.dataCollector');
+
+ENABLE_CAM_BUTTON.addEventListener('click', enableCam);
+TRAIN_BUTTON.addEventListener('click', trainAndPredict);
+RESET_BUTTON.addEventListener('click', reset);
+
+function hasGetUserMedia() {
+    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+  }
+  
+  function enableCam() {
+    if (hasGetUserMedia()) {
+      // getUsermedia parameters.
+      const constraints = {
+        video: true,
+        width: 640, 
+        height: 480 
+      };
+  
+      // Activate the webcam stream.
+      navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+        VIDEO.srcObject = stream;
+        VIDEO.addEventListener('loadeddata', function() {
+          videoPlaying = true;
+          ENABLE_CAM_BUTTON.classList.add('removed');
+        });
+      });
+    } else {
+      console.warn('getUserMedia() is not supported by your browser');
+    }
+  }
+
+/**
+ * Loads the MobileNet model and warms it up so ready for use.
+ **/
+async function loadMobileNetFeatureModel() {
+    const URL = MODEL_URL;
+    
+    mobilenet = await tf.loadGraphModel(URL, {fromTFHub: true});
+    STATUS.innerText = 'Model loaded successfully';
+    
+    // Warm up the model by passing zeros through it once.
+    tf.tidy(function () {
+      let answer = mobilenet.predict(tf.zeros([1, MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH, 3]));
+      console.log(answer.shape);
+    });
+}
+  
+  // Call the function immediately to start loading.
+loadMobileNetFeatureModel();
+
+function predictLoop() {
+    if (predict) {
+      tf.tidy(function() {
+        let videoFrameAsTensor = tf.browser.fromPixels(VIDEO).div(255);
+        let resizedTensorFrame = tf.image.resizeBilinear(videoFrameAsTensor,[MOBILE_NET_INPUT_HEIGHT, 
+            MOBILE_NET_INPUT_WIDTH], true);
+  
+        let imageFeatures = mobilenet.predict(resizedTensorFrame.expandDims());
+        let prediction = model.predict(imageFeatures).squeeze();
+        let highestIndex = prediction.argMax().arraySync();
+        let predictionArray = prediction.arraySync();
+
+        STATUS.innerText = 'Prediction: ' + CLASS_NAMES[highestIndex] + ' with ' + Math.floor(predictionArray[highestIndex] * 100) + '% confidence';
+      });
+  
+      window.requestAnimationFrame(predictLoop);
+    }
+  }
