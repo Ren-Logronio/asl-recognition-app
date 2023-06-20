aa63a6e5357bbbf71792dd47515fcff52f4a85e6
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2023-06-20 14:40:39 +0800

    python image preprocessing

--- model/pre-processing/pre-process.py
+++ model/pre-processing/pre-process.py
@@ -1,24 +1,60 @@
 import cv2
-import glob
+import os
+import numpy as np
 
-# select path
-path = ''
-image_number = 1
+orb = cv2.ORB_create()
+total_images = 0
+# prompt target path
+target_path = input("Enter target path: ")
 
 def process_image(image):
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return cv2.Canny(image_gray, threshold1=100, threshold2=200) 
+    frame_height, frame_width, _ = image.shape
+    frame_size = min(frame_height, frame_width)
+    frame_x = (frame_width - frame_size) // 2
+    frame_y = (frame_height - frame_size) // 2
+    cropped_frame = image[frame_y:frame_y+frame_size, frame_x:frame_x+frame_size]
+    resized_frame = cv2.resize(cropped_frame, (220, 220), interpolation= cv2.INTER_LINEAR)
+    normalized_image = np.zeros((220, 220))
+    normalized_image = cv2.normalize(resized_frame,  normalized_image, 0, 255, cv2.NORM_MINMAX)
+    grayed_image = cv2.cvtColor(normalized_image, cv2.COLOR_BGR2GRAY)
+    orb_detection = orb.detect(grayed_image, None)
+    orb_detection, orb_descriptors = orb.compute(grayed_image, orb_detection)
+    output = cv2.drawKeypoints(grayed_image, orb_detection, None, color=(0, 255, 0), flags=0)
+    return output
 
-# THE BATCH PROCESSING PART
-for file in enumerate(glob.glob(path), start=1):
-    image = cv2.imread(file, flags=0) 
-    print(f"Reading Image: {file} @ {image_number}")
+# delete path in the target folder with any '!' at the end, include the files in the folder
+for folder in os.listdir(target_path):
+    if folder.endswith('!'):
+        for file in os.listdir(os.path.join(target_path, folder)):
+            os.remove(os.path.join(target_path, folder, file))
+        os.rmdir(os.path.join(target_path, folder))
 
-    image_processed = process_image(image) 
-    print(f'Successfully processed image: {file} @ {image_number}')
-    
-    cv2.imwrite(f"{str(image_number)}.jpg", image_processed) 
-    print(f'Successfully saved processed image: {file} @ {image_number}')
-    image_number +=1
-
-print(f'Processed {image_number} images')
+# for every folder in path\
+for folder in os.listdir(target_path):
+    # print that you are selecting the folder
+    print(f'Selecting folder {folder}')
+    image_number = 1
+    # add ! at the end of output path and create it
+    output_path = os.path.join(target_path, folder + '!')
+    # check if output_path exists
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    # for every image in folder
+    for file in os.listdir(os.path.join(target_path, folder)):
+        if not file.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+            continue
+        image = cv2.imread(os.path.join(target_path, folder, file))
+        processed_image = process_image(image)
+        cv2.imwrite(os.path.join(output_path, f'{image_number}.jpg'), processed_image)
+        # print the image_number and the image path
+        print(f'Processed image {image_number} in {output_path}')
+        image_number += 1
+        total_images += 1
+'''
+for file in os.listdir(target_path):
+    image = cv2.imread(os.path.join(target_path, file))
+    processed_image = process_image(image)
+    cv2.imwrite(os.path.join(output_path, f'{image_number}.jpg'), processed_image)
+    image_number += 1
+'''
+print(f'Processed {total_images} images')
--- model/pre-processing/webcam.py
+++ model/pre-processing/webcam.py
@@ -1,5 +1,12 @@
 import numpy as np
 import cv2 as cv
+image_index = 1
+image_low = 80
+image_high = 130
+
+
+orb = cv.ORB_create()
+
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
@@ -7,17 +14,43 @@ if not cap.isOpened():
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
+    frame_height, frame_width, _ = frame.shape
+    frame_size = min(frame_height, frame_width)
+    frame_x = (frame_width - frame_size) // 2
+    frame_y = (frame_height - frame_size) // 2
+    cropped_frame = frame[frame_y:frame_y+frame_size, frame_x:frame_x+frame_size]
+    resized_frame = cv.resize(cropped_frame, (220, 220), interpolation= cv.INTER_LINEAR)
+    normalized_image = np.zeros((220, 220))
+    normalized_image = cv.normalize(resized_frame,  normalized_image, 0, 255, cv.NORM_MINMAX)
+    grayed_image = cv.cvtColor(normalized_image, cv.COLOR_BGR2GRAY)
+    orb_detection = orb.detect(grayed_image, None)
+    orb_detection, orb_descriptors = orb.compute(grayed_image, orb_detection)
+    output = cv.drawKeypoints(grayed_image, orb_detection, None, color=(0, 255, 0), flags=0)
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
     # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    canny = cv.Canny(gray, threshold1=100, threshold2=200)
+    
+
     # Display the resulting frame
-    cv.imshow('frame', canny)
+    cv.imshow('frame', output)
+    '''
+    if cv.waitKey(10) == ord('w'):
+        image_low +=1
+    if cv.waitKey(10) == ord('s'):
+        image_low -=1
+    if cv.waitKey(10) == ord('e'):
+        image_high +=1
+    if cv.waitKey(10) == ord('d'):
+        image_high -=1
+    if cv.waitKey(10) == ord('c'):
+        print(f"Low Threshold: {image_low}, High Threshold: {image_high}")
+    '''
+    if cv.waitKey(25) == ord('r'):
+        cv.imwrite(f'{image_index}.jpg', output)
+        image_index += 1
     if cv.waitKey(1) == ord('q'):
         break
-# When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
